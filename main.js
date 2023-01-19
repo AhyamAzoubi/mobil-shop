fb00aa8df5faf39909821998e02cf49bba9fc898
AhyamAzoubi <101559876+AhyamAzoubi@users.noreply.github.com>
2023-01-19 14:19:17 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const img = document.querySelector(".img");
+const container = document.querySelector(".container");
+const phones = (phone) => {
+  img.src = phone;
+};
+
+const colors = (color) => {
+  container.style.background = color;
+};
